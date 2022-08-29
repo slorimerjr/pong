a9e0d3c41e1485ed80abbe8a8b8db716e6e59db0
slorimerjr <slorimerjr@gmail.com>
2022-08-29 11:52:21 -0500

    added scoreboard, point tracking

--- ball.py
+++ ball.py
@@ -23,9 +23,15 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
 
-    def bounce_paddle(self):
+    def bounce_x(self):
         self.x_move *= -1
+
+    # gets ball to reset it's position and reverse the x axis to give other player the serve:
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+
 
